f4c08ccb7884c3b4d3ac0efe01dddcccff82e201
Mateusz Wojtczak <mateusz.wojtczak@leancode.pl>
2021-11-05 14:12:38 +0100

    Add sample0

--- labs/week5_lab/lib/main.dart
+++ labs/week5_lab/lib/main.dart
@@ -5,8 +5,9 @@ import 'package:week5_lab/sample/3/sample.dart';
 import 'package:week5_lab/sample/4/sample.dart';
 
 void main() {
+  runApp(const Sample0());
   // runApp(const Sample());
   // runApp(const Sample2());
   // runApp(const Sample3());
-  runApp(const Sample4());
+  // runApp(const Sample4());
 }
