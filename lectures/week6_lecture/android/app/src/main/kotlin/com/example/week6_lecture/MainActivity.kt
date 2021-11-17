0163f043b08fc36bea9828b2f9ec11f54214b8f1
Mateusz Wojtczak <mateusz.wojtczak@leancode.pl>
2021-11-17 01:19:43 +0100

    Add week6_lecture with dog facts

---
+++ lectures/week6_lecture/android/app/src/main/kotlin/com/example/week6_lecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.week6_lecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lectures/week6_lecture/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:week6_lecture/dog_facts/dog_facts_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Builder(builder: (context) {
+        return Scaffold(
+          body: ListView(
+            children: [
+              ElevatedButton(
+                onPressed: () =>
+                    Navigator.of(context).push(DogFactsPage.route()),
+                child: const Text('Dog facts'),
+              )
+            ],
+          ),
+        );
+      }),
+    );
+  }
+}
