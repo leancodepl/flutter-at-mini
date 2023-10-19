fe523a32fcd4ba29cbf6d4a14ab506655bca63f5
Tom Koter <kotertom@gmail.com>
2023-10-20 01:47:59 +0200

    Add week 3 lab boilerplate

---
+++ labs/week3_lab/lib/genre_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GenreScreen extends StatelessWidget {
+  const GenreScreen({super.key, required this.genreId});
+
+  final String genreId;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
