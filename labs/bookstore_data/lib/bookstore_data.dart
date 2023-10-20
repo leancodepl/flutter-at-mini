e0463797a360b985f5fc293a7a72c5c5330d0cbb
Tom Koter <kotertom@gmail.com>
2023-10-20 11:35:15 +0200

    Add getBook, getAuthor and getGenre helpers for lab 3

--- labs/bookstore_data/lib/bookstore_data.dart
+++ labs/bookstore_data/lib/bookstore_data.dart
@@ -1,5 +1,6 @@
 import 'package:bookstore_data/authors.dart' as a;
 import 'package:bookstore_data/books.dart' as b;
+import 'package:bookstore_data/entities.dart';
 import 'package:bookstore_data/genres.dart' as g;
 
 export 'entities.dart';
@@ -8,4 +9,28 @@ abstract class Bookstore {
   static final books = b.books;
   static final authors = a.authors;
   static final genres = g.genres;
+
+  Book getBook({required String id}) {
+    if (id.startsWith(':')) {
+      throw ArgumentError(
+          '`:` nie jest częścią `id`. To tylko marker w go_routerze, który oznacza, że to jest parametr');
+    }
+    return books.firstWhere((book) => book.id == id);
+  }
+
+  Author getAuthor({required String id}) {
+    if (id.startsWith(':')) {
+      throw ArgumentError(
+          '`:` nie jest częścią `id`. To tylko marker w go_routerze, który oznacza, że to jest parametr');
+    }
+    return authors.firstWhere((author) => author.id == id);
+  }
+
+  Genre getGenre({required String id}) {
+    if (id.startsWith(':')) {
+      throw ArgumentError(
+          '`:` nie jest częścią `id`. To tylko marker w go_routerze, który oznacza, że to jest parametr');
+    }
+    return genres.firstWhere((genre) => genre.id == id);
+  }
 }
