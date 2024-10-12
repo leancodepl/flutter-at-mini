7010305faa9080f580a48d0b49510dd84f5958d9
PiotrRogulski <piter.rogulski@gmail.com>
2024-10-12 13:15:50 +0200

    Restore example file

--- lectures/week1_lecture/13_patterns_with_classes.dart
+++ lectures/week1_lecture/13_patterns_with_classes.dart
@@ -4,33 +4,14 @@ void printStatus(PaymentStatus status) {
   switch (status) {
     case PaymentSuccess(transactionId: var t):
       print('success with id: $t');
-    case PaymentFailure(errorCode: int code, :final errorDescription):
+    case PaymentFailure(errorCode: int code):
       print('failure with code: $code');
     case PaymentPending():
       print('still pending');
   }
 }
 
-final failure = PaymentFailure(errorCode: 123, errorDescription: 'hello');
-
 void main() {
-  // without:
-  final code = failure.errorCode;
-  final errorDescription = failure.errorDescription;
-
-  // with:
-  final PaymentFailure(
-    errorCode: code,
-    errorDescription: description,
-  ) = failure;
-
-  
-  final PaymentFailure(
-    :errorCode,
-    :errorDescription,
-  ) = failure;
-
-
   printStatus(PaymentSuccess(transactionId: '123456'));
   printStatus(
     PaymentFailure(
