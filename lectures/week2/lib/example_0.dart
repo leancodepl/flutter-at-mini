b5756b6d7d10975c609eef6a2b15c5a60e3347fe
Mateusz Wojtczak <mateusz.wojtczak@leancode.pl>
2024-10-15 17:49:45 +0200

    Add week1 and week2 lectures

---
+++ lectures/week2/lib/example_0.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Example0 extends StatelessWidget {
+  const Example0({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Hello World!');
+  }
+}
