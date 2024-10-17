5d617f87d2927676ca8131255c25d31ed14c856c
PiotrRogulski <piter.rogulski@gmail.com>
2024-10-18 00:32:45 +0200

    Add week1 solutions

--- labs/week1/lib/part1.dart
+++ labs/week1/lib/part1.dart
@@ -1,3 +1,42 @@
+import 'dart:io';
+
 void part1() {
   print('\n=== Part 1: Hello, Dart! ===');
+
+  greeting();
+
+  triangle1();
+  triangle2();
+  triangle3();
+}
+
+void greeting() {
+  print("What's your name?");
+  final name = stdin.readLineSync();
+
+  print("What's your favorite color?");
+  final color = stdin.readLineSync();
+
+  print('Hello, $name! Your favorite color is $color.');
+}
+
+const count = 7;
+
+void triangle1() {
+  for (var i = 0; i < count; i++) {
+    for (var j = 0; j <= i; j++) {
+      stdout.write('*');
+    }
+    stdout.writeln();
+  }
+}
+
+void triangle2() {
+  for (var i = 1; i <= count; i++) {
+    print('*' * i);
+  }
+}
+
+void triangle3() {
+  print(Iterable.generate(count, (i) => '*' * (i + 1)).join('\n'));
 }
--- labs/week1/lib/part5.dart
+++ labs/week1/lib/part5.dart
@@ -1,3 +1,37 @@
+import 'dart:math';
+
 void part5() {
   print('\n=== Part 5: Records ===');
+
+  final triple = generate();
+  final result = transform(triple);
+  print('Generated: $triple');
+  print('Transformed: $result');
+
+  final (:x, :y, :enabled, :temperature) = randomize();
+  print('Randomized: x=$x, y=$y, enabled=$enabled, temperature=$temperature');
 }
+
+final random = Random();
+
+(int, int, int) generate() => (
+      random.nextInt(20),
+      random.nextInt(20),
+      random.nextInt(20),
+    );
+
+Object transform((int, int, int) triple) => switch (triple) {
+      (0, 0, 0) => 'All zeros!',
+      (> 10, 5, final c) => c,
+      (final a, final b, final c) when a.isEven => (b, c),
+// or, to check without the `when` clause:
+      (int(isEven: true), final b, final c) => (b, c),
+      final triple => triple,
+    };
+
+({int x, int y, bool enabled, double temperature}) randomize() => (
+      x: random.nextInt(100),
+      y: random.nextInt(100),
+      enabled: random.nextBool(),
+      temperature: random.nextDouble() * 100,
+    );
