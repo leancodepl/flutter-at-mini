a5063534a30207f1f8caa9f0cba90cae1f1e8489
PiotrRogulski <piter.rogulski@gmail.com>
2024-10-19 16:13:56 +0200

    Bring over previous week3 files

---
+++ labs/week3/lib/author_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AuthorScreen extends StatelessWidget {
+  const AuthorScreen({super.key, required this.authorId});
+
+  final String authorId;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- labs/week3/lib/main.dart
+++ labs/week3/lib/main.dart
@@ -1,20 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:week3/author_screen.dart';
+import 'package:week3/book_list_screen.dart';
+import 'package:week3/book_screen.dart';
+import 'package:week3/genre_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp.router(
+      routerConfig: _router,
+      title: 'Week 3',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.yellow,
+          dynamicSchemeVariant: DynamicSchemeVariant.fidelity,
         ),
       ),
     );
   }
 }
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const BookListScreen(),
+      routes: [
+        GoRoute(
+          path: 'book/:bookId',
+          builder: (context, state) => BookScreen(
+            bookId: state.pathParameters['bookId']!,
+          ),
+        ),
+        GoRoute(
+          path: 'author/:authorId',
+          builder: (context, state) => AuthorScreen(
+            authorId: state.pathParameters['authorId']!,
+          ),
+        ),
+        GoRoute(
+          path: 'genre/:genreId',
+          builder: (context, state) => GenreScreen(
+            genreId: state.pathParameters['genreId']!,
+          ),
+        ),
+      ],
+    ),
+  ],
+);
