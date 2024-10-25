df10af353a8ff84512f31075a61638180be7cf23
Piotr Rogulski <piotr.rogulski@leancode.pl>
2024-10-25 08:07:39 +0200

    Week 3 (#10)
    * Update lints in weeks 1 and 2
    
    * Bootstrap week 3 project
    
    * Add run configurations
    
    * Bring over previous week3 files
    
    * Bring over data store for week 3
    
    * Tweak data store definition, make models equatable
    
    * Add IntelliJ run configuration for week 2 lecture
    
    * Add task description
    
    * Tweak theme
    
    * Update README.md about responsiveness and navigation
    
    * Tweak go_router config and faked data
    
    * Update task description
    
    * Enable network for macOS project
    
    * Update README.md
    
    * Add disclaimer about formatting and linting
    
    * Add week 2 solutions

--- labs/week2/lib/task1.dart
+++ labs/week2/lib/task1.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
 class Task1 extends StatelessWidget {
@@ -5,6 +7,100 @@ class Task1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return switch (constraints.maxWidth) {
+          < 600 => const VerticalLayoutWidget(),
+          _ => const WideLayoutWidget(),
+        };
+      },
+    );
+  }
+}
+
+class VerticalLayoutWidget extends StatelessWidget {
+  const VerticalLayoutWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final BoxConstraints(:maxWidth, :maxHeight) = constraints;
+
+          return Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: math.max(maxHeight - 64, 0),
+              width: maxWidth,
+              color: Colors.red,
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  height: maxHeight / 2,
+                  color: Colors.green,
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      height: 64,
+                      width: 120,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class WideLayoutWidget extends StatelessWidget {
+  const WideLayoutWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final BoxConstraints(:maxWidth, :maxHeight) = constraints;
+
+          return Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: math.max(maxHeight - 64, 0),
+              width: maxWidth,
+              color: Colors.red,
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  width: math.max(
+                    maxWidth * 3 / 4,
+                    maxWidth - 300,
+                  ),
+                  color: Colors.green,
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      height: 64,
+                      width: 120,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
--- labs/week2/lib/task2.dart
+++ labs/week2/lib/task2.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Task2 extends StatelessWidget {
@@ -5,6 +7,35 @@ class Task2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final BoxConstraints(:maxWidth, :maxHeight) = constraints;
+
+        const count = 10;
+
+        Widget makeBox(int index, {Widget? child}) {
+          return Container(
+            width: maxWidth / pow(2, index),
+            height: maxHeight / pow(2, index),
+            color: Colors.red,
+            child: child,
+          );
+        }
+
+        var child = makeBox(count);
+
+        for (var i = count - 1; i > 0; i--) {
+          child = makeBox(
+            i,
+            child: Align(
+              alignment: const FractionalOffset(2, 2),
+              child: child,
+            ),
+          );
+        }
+
+        return child;
+      },
+    );
   }
 }
