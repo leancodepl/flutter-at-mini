8a25fda1e71e4d7b0d17f8f51fa084b1d2a61a06
Piotr Rogulski <piter.rogulski@gmail.com>
2025-10-16 22:08:50 +0200

    Add week 1 solution

--- labs/week1/lib/part4.dart
+++ labs/week1/lib/part4.dart
@@ -1,5 +1,27 @@
 void part4() {
   print('\n=== Part 4: Generics ===');
+
+  const containers = [
+    Container<int>(1),
+    Container<String>('Hello'),
+    Container<double>(3.14),
+    Container<bool>(true),
+    // the line below doesn't compile
+    // Container<int?>(null),
+  ];
+
+  for (final container in containers) {
+    switch (container) {
+      case Container<num>(:final value):
+        print('Got a number – the square is ${value * value}');
+      case Container<String>(:final value):
+        print('Got a string – the length is ${value.length}');
+    }
+  }
 }
 
-class Container {}
+class Container<T extends Object> {
+  const Container(this.value);
+
+  final T value;
+}
