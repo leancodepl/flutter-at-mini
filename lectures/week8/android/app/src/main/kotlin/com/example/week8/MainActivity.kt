01be93d0509f109f773c7e155a3fb1d54b709127
wzslr321 <wiktor.zajac888@gmail.com>
2025-12-01 15:09:51 +0100

    feat: raw week8 code structure

---
+++ lectures/week8/android/app/src/main/kotlin/com/example/week8/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.week8
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lectures/week8/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Scaffold(body: Center(child: Text('Hello, World!'))),
+    );
+  }
+}
